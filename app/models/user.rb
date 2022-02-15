633d7cd18811737f737f41506846e44432e01dad
Jescart Baculi <jescartbaculi@yahoo.com>
2022-02-15 18:12:41 +0800

    set devise controllers default response to json format

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :validatable,
-  devise :database_authenticatable,
-  :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
+  devise :database_authenticatable, :registerable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,7 @@ Rails.application.routes.draw do
   controllers: {
     registrations: 'registrations',
     sessions: 'sessions'
-  }
+  }, defaults: { format: :json }
 
-  get '/member-date', to: 'members#show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/member-data', to: 'members#show'
 end
